6653c896a6671790c631575b7be11a1b02776aba
JakeAsh22 <jacob.ash1998@gmail.com>
2019-12-13 14:58:04 -0800

    Will have user only able to input ints that are at least 0

--- program.cs
+++ program.cs
@@ -8,14 +8,24 @@ namespace Calc
     {
         public static void Main()
         {
+            int breadNum = 0;
+            int pastryNum = 0;
             Console.WriteLine("Please enter how much bread you would like to buy: ");
-            int bread = int.Parse(Console.ReadLine());
+            string bread = Console.ReadLine();
+            bool breadConvert = int.TryParse(bread, out breadNum);
             Console.WriteLine("Please enter how many pastries you would like to buy: ");
-            int pastry = int.Parse(Console.ReadLine());
-            Bread userBread = new Bread(bread);
-            Pastry userPastry = new Pastry(pastry);
+            string pastry = Console.ReadLine();
+            bool pastryConvert = int.TryParse(pastry, out pastryNum);
+            if (breadConvert && pastryConvert && breadNum>=0 && pastryNum>=0)
+            {
+            Bread userBread = new Bread(breadNum);
+            Pastry userPastry = new Pastry(pastryNum);
             Console.WriteLine("The total cost for bread is: $"+userBread.BreadCost()+".  The total cost for the pastries is: $"+userPastry.PastryCost());
             Console.WriteLine("Your Total is: $"+(userPastry.PastryCost()+userBread.BreadCost()));
+            }
+            else 
+            Console.WriteLine("Please enter only whole numbers that are at least 0");
+            
         }
     }
 }
